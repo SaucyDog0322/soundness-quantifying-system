0d7e7171995b5801c5988fef0b3b084bb962aa19
root <root@zabbix.matsue-ct.jp>
2023-02-16 08:54:22 +0900

    fix zabbix

---
+++ trans/traBitReceive.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#ファイルの転送
+# 下のコードではユーザ名をuser,宛先IPアドレスを1.1.1.1,パスワードをppaassssとしている
+  expect -c "
+  
+  spawn scp /root/data/BitReceive.csv user@1.1.1.1:/home/user/public_html/data
+  expect {
+    \"Are you sure you want to continue connecting (yes/no)?\" {
+      send \"yes\r\"
+      expect \"password:\"
+      send \"ppaassss\r\"
+    } \"password:\" {
+      send \"ppaassss\r\"
+    }
+  }
+expect {
+    \"denied\" { exit 0 } 
+    \"100%\" { exit 1 }
+  }  "
